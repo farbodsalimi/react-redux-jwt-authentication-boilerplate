ba9e9358109d53fafcdbecb1f7dd437367e83905
Farbod Salimi <farbod_salimi@yahoo.com>
2017-08-21 23:24:01 -0700

    Init

---
+++ server/router.js
@@ -0,0 +1,16 @@
+const Authentication = require('./controllers/authentication');
+const passwordService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
+
+module.exports = (app) => {
+
+    app.post('/signin', requireSignin, Authentication.signin);    
+    app.post('/signup', Authentication.signup);
+
+    app.get('/', requireAuth, (req, res, next) => {
+        res.send(['water', 'phone', 'paper']);
+    });
+};
