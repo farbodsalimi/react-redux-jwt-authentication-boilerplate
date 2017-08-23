f2ff24b6719a3370311024b3f1da802ce8a0448b
Farbod Salimi <farbod_salimi@yahoo.com>
2017-08-23 14:40:28 -0700

    Add models

---
+++ server/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+
+// define our model
+const userSchema = new Schema({
+    email: { type: String, unique: true, lowercase: true },
+    password: String
+});
+
+// on save hook, encrypt password
+// before saving a model, run this function
+
+userSchema.pre('save', function (next) {
+    // get access to the user model
+    const user = this;
+
+    // generate a salt then run callback
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) { return next(err); }
+
+        // hash our passwork using the salt
+        bcrypt.hash(user.password, salt, null, (err, hash) => {
+            if (err) { return next(err); }
+
+            // overwrite plain text password with encrypted password
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword, callback) {
+    bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+        if (err) { return callback(err); }
+        
+        callback(null, isMatch);
+    });
+};
+
+// create the model class
+const model = mongoose.model('user', userSchema);
+
+// export the model
+module.exports = model;
