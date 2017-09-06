c42e001c33fa8ae25b4e54733536152c54ac5e82
Farbod Salimi <farbod_salimi@yahoo.com>
2017-09-06 00:19:20 -0700

    Add header

---
+++ client/src/components/header.js
@@ -0,0 +1,43 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+class Header extends PureComponent {
+
+    renderLinks() {
+        if (this.props.authenticated) {
+            return (
+                <li className="nav-item">
+                    <Link className="nav-link" to="/signout">Sign Out</Link>
+                </li>
+            );
+        } else {
+            return [
+                <li className="nav-item" key="signin">
+                    <Link className="nav-link" to="/signin">Sign In</Link>
+                </li>,
+                <li className="nav-item" key="signup">
+                    <Link className="nav-link" to="/signup">Sign Up</Link>
+                </li>
+            ];
+        }
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+                <Link to="/" className="navbar-brand">Redux Auth</Link>
+
+                <ul className="navbar-nav mr-auto">
+                    {this.renderLinks()}
+                </ul>
+            </nav>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return { authenticated: state.auth.authenticated }
+};
+
+export default connect(mapStateToProps)(Header);
