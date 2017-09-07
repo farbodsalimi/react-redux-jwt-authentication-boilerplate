e2434cd0aa3b9e72e5ad44253407c2bed7d8d6fa
Farbod Salimi <farbod_salimi@yahoo.com>
2017-09-07 11:49:58 -0700

    Add feature component

---
+++ client/src/components/feature.js
@@ -0,0 +1,37 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class Feature extends PureComponent {
+
+    componentWillMount() {
+        this.props.fetchFeature();
+    }
+
+    renderFeature() {
+        return this.props.features.map(feature => {
+            return <li key={feature}>{feature}</li>;
+        })
+    }
+
+    render() {
+        if (!this.props.features) {
+            return <div>Loading...</div>;
+        }
+
+        return (
+            <div>
+                <div>Feature</div>
+                <ul>
+                    {this.renderFeature()}
+                </ul>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return { features: state.features.homePageFeatures }
+}
+
+export default connect(mapStateToProps, actions)(Feature);
